08b395c7b5649c369e648570fca5d13a613aabfc
xueguofeng <xueguofeng2011@gmail.com>
2020-02-16 17:07:54 -0800

    add function X and Y by Richard

---
+++ src/main/java/com/xgf/Mavenlearning/App.java
@@ -0,0 +1,20 @@
+package com.xgf.Mavenlearning;
+
+/**
+ * Hello world!
+ *
+ */
+
+public class App 
+{
+	public String sayHello()
+	{
+		return "Hello Maven ---------------- this is Richard from App";
+	}
+	
+    public static void main( String[] args )
+    {
+ 
+        System.out.println( new App().sayHello());
+    }
+}
